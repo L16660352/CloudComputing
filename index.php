dd2dc4191d3434baafe6ef3ff012f983622e282b
L16660352 <l16660352@matehuala.tecnm.mx>
2024-10-09 07:44:54 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>¡Bienvenido a PHP!</title>
+</head>
+<body>
+    <?php
+        echo "<h1>¡Bienvenido al mundo de la programación PHP!</h1>";
+    ?>
+</body>
+</html>
